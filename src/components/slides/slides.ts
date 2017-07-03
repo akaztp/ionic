074a2466c110bdeeacf8f1e72541ba9cc720084a
José Proença <joseantonioproenca@gmail.com>
2017-07-03 11:56:17 +0100

    fix(slides) add initialization to slides collection

--- src/components/slides/slides.ts
+++ src/components/slides/slides.ts
@@ -825,7 +825,7 @@ export class Slides extends Ion {
   /** @internal */
   _rtl: boolean;
   /** @internal */
-  _slides: SlideElement[];
+  _slides: SlideElement[] = [];
   /** @internal */
   _snapGrid: any;
   /** @internal */
